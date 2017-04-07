15f0e844e426006955359d718e8673e1ab69ee68
somkiat <somkiat.p@gmail.com>
2017-04-07 22:51:38 +0700

    Fix bug

--- runjenkins.sh
+++ runjenkins.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+if [[ -z $GITHUB_ID ]] ; then
+	echo "ERROR: GITHUB_ID environment variables must be set."
+	exit 1
+fi
+
+find /opt/jenkins/data -type f | xargs sed -i -e "s/@@GHUSER@@/${GITHUB_ID}/g"
 java -jar /opt/jenkins/jenkins.war
